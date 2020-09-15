8fc9d4c0d49af77bbc4d94a76c6c41c260637637
breadthe <omigoshdev@protonmail.com>
2020-09-15 17:26:17 -0500

    Refactor login form to components

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,48 +4,44 @@
 <section class="container mx-auto">
     <h1>{{ __('Login') }}</h1>
 
-    <form method="POST" action="{{ route('login') }}" class="p-4 border bg-white">
-        @csrf
+    <x-forms.form :action="route('login')" class="p-4 border bg-white">
 
-        <div class="form__group">
-            <label for="email" class="col-md-4 w-full sm:w-1/3 flex justify-start sm:justify-end px-0 sm:px-2">{{ __('E-Mail Address') }}</label>
-
-            <div class="w-full sm:w-1/2">
-                <input id="email" type="email" class="form__control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="form__group">
-            <label for="password" class="col-md-4 w-full sm:w-1/3 flex justify-start sm:justify-end px-0 sm:px-2">{{ __('Password') }}</label>
+        <x-forms.group>
+            <x-forms.label for="email" required>
+                {{ __('E-Mail Address') }}
+            </x-forms.label>
 
             <div class="w-full sm:w-1/2">
-                <input id="password" type="password" class="form__control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                <x-forms.input id="email" type="email" name="email" required autofocus/>
 
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
+                <x-forms.error field="email"/>
             </div>
-        </div>
+        </x-forms.group>
 
-        <div class="flex flex-row sm:form__group mt-8">
-            <div class="sm:ml-auto sm:w-2/3 flex justify-start items-center">
-                <input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+        <x-forms.group>
+            <x-forms.label for="password" required>
+                {{ __('Password') }}
+            </x-forms.label>
 
-                <label class="mb-0 ml-2 sm:ml-0 sm:w-1/2 px-0 sm:px-2" for="remember">
+
+            <div class="w-full sm:w-1/2">
+                <x-forms.input id="password" type="password" name="password" required/>
+
+                <x-forms.error field="password"/>
+            </div>
+        </x-forms.group>
+
+        <x-forms.group class="flex flex-row mt-8">
+            <div class="w-full sm:ml-auto sm:w-2/3 flex justify-start items-center">
+                <x-forms.input id="remember" type="checkbox" name="remember"/>
+
+                <x-forms.label for="remember" class="" inline>
                     {{ __('Remember Me') }}
-                </label>
+                </x-forms.label>
             </div>
-        </div>
+        </x-forms.group>
 
-        <div class="flex flex-row sm:form__group mt-8">
+        <x-forms.group class="flex flex-row mt-8">
             <div class="sm:ml-auto sm:w-2/3">
                 <button type="submit" class="btn btn--blue btn--pill">
                     {{ __('Login') }}
@@ -55,7 +51,8 @@
                     {{ __('Forgot Your Password?') }}
                 </a>
             </div>
-        </div>
-    </form>
+        </x-forms.group>
+
+    </x-forms.form>
 </section>
 @endsection
